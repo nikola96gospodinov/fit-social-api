d982a36b3b489426df7d4cf3f2faaa1eb04198a1
Nik Gospodinov <nikola96gospodinov@gmail.com>
2024-08-27 08:19:33 +0100

    route added for getting exercises by multiple ids

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { getExercises } from "./handlers/get-exercises.handlers";
 import { getExerciseById } from "./handlers/get-exercise-by-id.handler";
+import { getExercisesByIds } from "./handlers/get-exercises-by-ids.handler";
 
 const router = Router();
 
 router.route("/exercises").get(getExercises);
 
 router.route("/exercise/:id").get(getExerciseById);
+
+router.route("/exercises-by-ids").get(getExercisesByIds);
 
 export default router;

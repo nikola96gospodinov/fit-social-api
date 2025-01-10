a3ddd5975bf4dc178ed36210740a5bcb56658f3d
Nik Gospodinov <nikola96gospodinov@gmail.com>
2025-01-10 10:20:19 +0000

    removing unused routes

--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,7 @@
 import { Router } from "express";
-import { getExercises } from "./handlers/exercise/get-exercises.handlers";
-import { getExerciseById } from "./handlers/exercise/get-exercise-by-id.handler";
-import { getExercisesByIds } from "./handlers/exercise/get-exercises-by-ids.handler";
 import { searchGyms } from "./handlers/gym/search-gym.handler";
 
 const router = Router();
-
-router.route("/exercises").get(getExercises);
-
-router.route("/exercises/:id").get(getExerciseById);
-
-router.route("/exercises-by-ids").get(getExercisesByIds);
 
 router.route("/search-gyms").get(searchGyms);
 
